d5873df5313267d99c76d6b24457a1b47ddb37e0
Mikołaj Andrzejewski <mikolaj.andrzejewski91@wp.pl>
2018-12-05 11:26:26 +0100

    day 2 done

--- Day2/src/Main.java
+++ Day2/src/Main.java
@@ -10,31 +10,57 @@ public class Main {
 
         List<String> list = new ArrayList<>();
 
-        while (scanner.hasNext()){
+        while (scanner.hasNext()) {
             list.add(scanner.next());
         }
 
         System.out.println(partOne(list));
+
+        partTwo(list);
+
+
     }
 
-    private static Integer partOne(List<String> list){
+    private static Integer partOne(List<String> list) {
 
         Set<String> duuble = new HashSet<>();
         Set<String> triple = new HashSet<>();
         int result = 0;
 
-        for(int i =0; i<list.size(); i++){
-            for(int j=0; j<list.get(i).length(); j++){
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.get(i).length(); j++) {
                 int finalJ = j;
                 int finalI = i;
                 long count = list.get(i).chars().filter(ch -> ch == list.get(finalI).charAt(finalJ)).count();
-                if(count == 2){
+                if (count == 2) {
                     duuble.add(list.get(i));
-                } else if (count == 3){
+                } else if (count == 3) {
                     triple.add(list.get(i));
                 }
             }
-        } result = duuble.size()*triple.size();
+        }
+        result = duuble.size() * triple.size();
         return result;
     }
+
+    private static void partTwo(List<String> list) {
+
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                String s1 = list.get(i);
+                String s2 = list.get(j);
+                StringBuilder sb = new StringBuilder();
+                for (int k = 0; k < s1.length(); k++) {
+                    if (s1.charAt(k) == s2.charAt(k)) {
+                        sb.append(s1.charAt(k));
+                    }
+                }
+                result.add(sb.toString());
+            }
+        }
+        result.stream().max(Comparator.comparing(String::length)).ifPresent(System.out::println);
+    }
 }
+
